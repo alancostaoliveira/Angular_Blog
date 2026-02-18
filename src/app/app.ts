0f3f56e84ab593d8cc5fd6c1bef846634bda8c62
Alan Costa Oliveira <alancostaoliveira@hotmail.com>
2026-02-18 02:36:17 -0300

    components createds with styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCard } from './components/small-card/small-card';
+import { BigCard } from './components/big-card/big-card';
+import { MenuTitle } from './components/menu-title/menu-title';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, BigCard],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Angular_Blog');
---
+++ src/app/components/big-card/big-card.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  imports: [],
+  templateUrl: './big-card.html',
+  styleUrl: './big-card.css',
+})
+export class BigCard {
+  @Input()
+  photoCover: string = '';
+}
